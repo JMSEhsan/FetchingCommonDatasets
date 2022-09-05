eeea196d5c1f8dbcb75ee5ec656fbe09701844a3
Ehsan Jalali <JMSEhsan@gmail.com>
2022-09-04 23:53:07 -0600

    first commit

---
+++ FetchingCommonDatasets.py
@@ -0,0 +1,11 @@
+# Source: Mueller, John Paul,"Functional Programming", for dummies, 2019
+
+from ast import increment_lineno
+from sklearn.datasets import fetch_olivetti_faces
+data = fetch_olivetti_faces()
+print(data.images.shape)
+
+import matplotlib.pyplot as plt
+#%matplotlib inline
+plt.imshow(data.images[1], cmap="gray")
+plt.show()
